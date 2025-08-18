2308fa4ed11abcfa504e99ea8f15bfcb9187a077
mia0205 <2322397131@qq.com>
2025-08-18 21:58:47 +0800

    更换头像-预览完成

--- src/views/user/userImage.vue
+++ src/views/user/userImage.vue
@@ -1,11 +1,82 @@
 <template>
-  <div>userimages</div>
+<div>
+  <el-card class="box-card">
+       <div slot="header" class="clearfix">
+         <span>更换头像</span>
+
+       </div>
+       <div class="text item">
+        <img v-if="avatar === '' " src="" alt="">
+        <img v-else :src="avatar" alt="">
+        <div class="btn">
+          <input type="file" accept="images/*" style="display: none;" @change="onFileChange" ref="iptRef">
+          <el-button type="primary" @click="chooseImg">
+          <i class="el-icon-plus">选择图片</i>
+        </el-button>
+        <el-button type="success">
+          <i class="el-icon-cloudy">上传头像</i>
+        </el-button>
+        </div>
+
+       </div>
+
+    </el-card>
+
+</div>
+
 </template>
 
 <script>
+// @change="onFileChange"
 export default {
+  data () {
+    return {
+      avatar: ''
+    }
+  },
+  methods: {
+    chooseImg () {
+      this.$refs.iptRef.click()
+    },
+    // 选择图片确定,获取用户选择的图片
+    onFileChange (e) {
+      const files = e.target.files
+      if (files.length === 0) {
+        this.avatar = ''
+      } else {
+        // 默认只能选一个
+        // console.log(files[0])
+        // 文件转内存临时地址
+        // URL.createObjectURL(文件)
+        // 把文件转成base64文件，可以发给后台,new FileReader().readAsDataURL(FILES[0]).onload=>e=>e.target.result
+        const fr = new FileReader()
+        fr.readAsDataURL(files[0])
+        // onload等待把文件读成base64字符串后会触发onload事件函数
+        fr.onload = e => {
+          this.avatar = e.target.result
+        }
+      }
+    }
+  }
 
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.box-card{
+  height: 70%;
+}
+.text{
+  height: 400px;
+  img{
+    width: 300px;
+    height: 300px;
+    margin-bottom: 20px;
+  }
+}
+
+.btn{
+  margin-bottom:10px;
+}
+
+</style>
