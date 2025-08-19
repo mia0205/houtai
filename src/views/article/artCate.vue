890d1b2a632b62ec408c02d7c4c31aaf73a6baac
mia0205 <2322397131@qq.com>
2025-08-19 21:19:41 +0800

    文章分类添加表单

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -2,7 +2,7 @@
   <el-card class="box-card">
   <div slot="header" class="clearfix">
     <span>文章分类</span>
-    <el-button style="float: right; padding: 6px 8px;" type="primary">添加分类</el-button>
+    <el-button style="float: right; padding: 6px 8px;" type="primary" @click="addCateFn">添加分类</el-button>
   </div>
   <div class="text item">
     <el-table
@@ -20,6 +20,29 @@
     </el-table>
 
   </div>
+  <!-- 添加文字分类 -->
+  <el-dialog
+  title="添加文章分类"
+  :visible.sync="addVisible"
+  width="35%"
+  @close="dialogCloseFn"
+  >
+
+    <el-form :model="ruleForm" ref="addForm" label-width="100px" class="demo-ruleForm" :rule="rules">
+  <el-form-item label="分类名称" prop="name">
+    <el-input  v-model="ruleForm.name"></el-input>
+  </el-form-item>
+  <el-form-item label="分类别名" prop="nickname">
+    <el-input v-model="ruleForm.nickname"></el-input>
+  </el-form-item>
+
+  </el-form>
+
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="addVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
 </el-card>
 </template>
 
@@ -29,7 +52,22 @@ import { getArtCateAPI } from '@/api'
 export default {
   data () {
     return {
-      cateList: []
+      cateList: [],
+      addVisible: false,
+      ruleForm: {
+        name: '',
+        nickname: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入用户名称', trigger: 'blur' },
+          { pattern: /^[a-zA-Z0-9]{1,10}$/, message: '用户名必须是1-10的大小写字母数字', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: true, message: '请输入用户昵称', trigger: 'blur' },
+          { pattern: /^[a-zA-Z0-9]{1,10}$/, message: '用户名必须是1-10的大小写字母数字', trigger: 'blur' }
+        ]
+      }
 
     }
   },
@@ -38,7 +76,16 @@ export default {
       const res = await getArtCateAPI()
 
       this.cateList = res.data.data
+    },
+    addCateFn () {
+      this.addVisible = true
+    },
+    dialogCloseFn () {
+      // this.ruleForm.name = ''
+      // this.ruleForm.nickname = ''
+      this.$refs.addForm.resetFields()
     }
+
   },
   created () {
     this.getArtCateFn()
@@ -47,4 +94,9 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.content{
+  margin-left:5px;
+}
+
+</style>
